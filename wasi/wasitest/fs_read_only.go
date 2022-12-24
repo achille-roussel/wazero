b6f56cf2b7e89230e97607f1532e10f711528525
Achille Roussel <achille@segment.com>
2022-12-24 15:45:52 +0000

    split wasitest/fs.go

---
+++ wasi/wasitest/fs_read_only.go
@@ -0,0 +1,145 @@
+package wasitest
+
+import (
+	"io/fs"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	"github.com/tetratelabs/wazero/wasi"
+)
+
+// MakeReadOnlyFS is a function type used to instentiate read-only file systems
+// during tests.
+type MakeReadOnlyFS func(fs.FS) (wasi.FS, CloseFS, error)
+
+// TestReadOnlyFS implements a test suite which validate that read-only
+// implementations of the wasi.FS interface behave according to the spec.
+func TestReadOnlyFS(t *testing.T, newFS MakeReadOnlyFS) {
+	t.Run("root", func(t *testing.T) {
+		testReadOnlyFS(t, newFS)
+	})
+	t.Run("sub", func(t *testing.T) {
+		testReadOnlyFS(t, func(files fs.FS) (wasi.FS, CloseFS, error) {
+			mapFiles := files.(fstest.MapFS)
+			subFiles := make(fstest.MapFS, len(mapFiles))
+			for name, file := range mapFiles {
+				subFiles["mnt/subfs/"+name] = file
+			}
+			baseFS, closeFS, err := newFS(subFiles)
+			if err != nil {
+				return nil, nil, err
+			}
+			if len(subFiles) == 0 {
+				return baseFS, closeFS, nil
+			}
+			subFS, err := wasi.Sub(baseFS, "mnt/subfs")
+			if err != nil {
+				closeFS()
+				return nil, nil, err
+			}
+			return subFS, closeFS, nil
+		})
+	})
+}
+
+func testReadOnlyFS(t *testing.T, newFS MakeReadOnlyFS) {
+	tests := []struct {
+		scenario string
+		function func(*testing.T, MakeReadOnlyFS)
+	}{
+		{
+			scenario: "create a file system with an empty state",
+			function: testReadOnlyFSCreateEmpty,
+		},
+
+		{
+			scenario: "opening a file which does not exist gives fs.ErrNotExist",
+			function: testReadOnlyFSOpenNotExist,
+		},
+
+		{
+			scenario: "existing files can be open and read",
+			function: testReadOnlyFSOpenAndRead,
+		},
+
+		{
+			scenario: "creating a directory errors with wasi.ErrReadOnly",
+			function: testReadOnlyFSCreateDirectory,
+		},
+
+		{
+			scenario: "setting file times errors with wasi.ErrReadOnly",
+			function: testReadOnlyFSSetFileTimes,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.scenario, func(t *testing.T) { test.function(t, newFS) })
+	}
+}
+
+func testReadOnlyFSCreateEmpty(t *testing.T, newFS MakeReadOnlyFS) {
+	fsys, closeFS := assertNewFS(t, readOnlyFS(newFS, nil))
+	defer assertCloseFS(t, closeFS)
+	testFS(t, fsys, nil)
+}
+
+func testReadOnlyFSOpenNotExist(t *testing.T, newFS MakeReadOnlyFS) {
+	fsys, closeFS := assertNewFS(t, readOnlyFS(newFS, nil))
+	defer assertCloseFS(t, closeFS)
+
+	_, err := fsys.OpenFile("/test", 0, 0)
+	assertErrorIs(t, err, fs.ErrNotExist)
+}
+
+func testReadOnlyFSOpenAndRead(t *testing.T, newFS MakeReadOnlyFS) {
+	now := time.Now()
+
+	files := fstest.MapFS{
+		"file-0": readOnlyFile(now, `Hello World!`),
+		"file-1": readOnlyFile(now, `42`),
+		"file-2": readOnlyFile(now, ``),
+	}
+
+	fsys, closeFS := assertNewFS(t, readOnlyFS(newFS, files))
+	defer assertCloseFS(t, closeFS)
+
+	assertPathData(t, fsys, "file-0", `Hello World!`)
+	assertPathData(t, fsys, "file-1", `42`)
+	assertPathData(t, fsys, "file-2", ``)
+
+	testFS(t, fsys, files)
+}
+
+func testReadOnlyFSCreateDirectory(t *testing.T, newFS MakeReadOnlyFS) {
+	fsys, closeFS := assertNewFS(t, readOnlyFS(newFS, nil))
+	defer assertCloseFS(t, closeFS)
+
+	err := fsys.CreateDir("tmp", 0755)
+	assertErrorIs(t, err, wasi.ErrReadOnly)
+}
+
+func testReadOnlyFSSetFileTimes(t *testing.T, newFS MakeReadOnlyFS) {
+	now := time.Now()
+
+	fsys, closeFS := assertNewFS(t, readOnlyFS(newFS, fstest.MapFS{
+		"hello": readOnlyFile(now, "world"),
+	}))
+	defer assertCloseFS(t, closeFS)
+
+	err := fsys.SetFileTimes("hello", 0, now.Add(time.Second), now)
+	assertErrorIs(t, err, wasi.ErrReadOnly)
+}
+
+func readOnlyFS(newFS MakeReadOnlyFS, files fstest.MapFS) func() (wasi.FS, CloseFS, error) {
+	return func() (wasi.FS, CloseFS, error) { return newFS(files) }
+}
+
+func readOnlyFile(modTime time.Time, data string) *fstest.MapFile {
+	return &fstest.MapFile{
+		Data:    []byte(data),
+		Mode:    0444,
+		ModTime: modTime,
+	}
+}
---
+++ wasi/wasitest/fs_read_write.go
@@ -0,0 +1,222 @@
+package wasitest
+
+import (
+	"io"
+	"io/fs"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	"github.com/tetratelabs/wazero/wasi"
+)
+
+// MakeReadWriteFS is a function type used to instantiate read-write file
+// systems during tests.
+type MakeReadWriteFS func() (wasi.FS, CloseFS, error)
+
+// TestReadOnlyFS implements a test suite which validate that read-write
+// implementations of the wasi.FS interface behave according to the spec.
+func TestReadWriteFS(t *testing.T, newFS MakeReadWriteFS) {
+	t.Run("root", func(t *testing.T) {
+		testReadWriteFS(t, newFS)
+	})
+	t.Run("sub", func(t *testing.T) {
+		testReadWriteFS(t, func() (wasi.FS, CloseFS, error) {
+			baseFS, closeFS, err := newFS()
+			if err != nil {
+				return nil, nil, err
+			}
+			if err := baseFS.CreateDir("mnt", 0755); err != nil {
+				closeFS()
+				return nil, nil, err
+			}
+			subFS, err := wasi.Sub(baseFS, "mnt")
+			if err != nil {
+				closeFS()
+				return nil, nil, err
+			}
+			return subFS, closeFS, nil
+		})
+	})
+}
+
+func testReadWriteFS(t *testing.T, newFS MakeReadWriteFS) {
+	tests := []struct {
+		scenario string
+		function func(*testing.T, MakeReadWriteFS)
+	}{
+		{
+			scenario: "create a file system with an empty state",
+			function: testReadWriteFSCreateEmpty,
+		},
+
+		{
+			scenario: "the file system can create directories",
+			function: testReadWriteFSCreateDirectory,
+		},
+
+		{
+			scenario: "the file system can create sub-directories",
+			function: testReadWriteFSCreateSubDirectory,
+		},
+
+		{
+			scenario: "creating an existing directory errors with fs.ErrExist",
+			function: testReadWriteFSCreateExistingDirectory,
+		},
+
+		{
+			scenario: "permissions are set on directories",
+			function: testReadWriteFSCreateDirectoryHasPermissions,
+		},
+
+		{
+			scenario: "files are created when opened with wasi.O_CREATE",
+			function: testReadWriteFSCreateFileWithOpen,
+		},
+
+		{
+			scenario: "files cannot be recreated when opened with wasi.O_EXCL",
+			function: testReadWriteFSCannotCreateExistingFile,
+		},
+
+		{
+			scenario: "files can be truncated when opened with wasi.O_TRUNC",
+			function: testReadWriteFSTruncateFileWithOpen,
+		},
+
+		{
+			scenario: "set access time on file",
+			function: testReadWriteFSSetFileAccessTime,
+		},
+
+		{
+			scenario: "set modification time on file",
+			function: testReadWriteFSSetFileModTime,
+		},
+
+		{
+			scenario: "set access and modification times on file",
+			function: testReadWriteFSSetFileTimes,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.scenario, func(t *testing.T) { test.function(t, newFS) })
+	}
+}
+
+func testReadWriteFSCreateEmpty(t *testing.T, newFS MakeReadWriteFS) {
+	_, closeFS := assertNewFS(t, newFS)
+	assertCloseFS(t, closeFS)
+}
+
+func testReadWriteFSCreateDirectory(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	assertCreateDir(t, fsys, "etc")
+	assertCreateDir(t, fsys, "var")
+	assertCreateDir(t, fsys, "tmp")
+
+	testFS(t, fsys, fstest.MapFS{
+		"etc": nil,
+		"var": nil,
+		"tmp": nil,
+	})
+}
+
+func testReadWriteFSCreateSubDirectory(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	assertCreateDir(t, fsys, "1")
+	assertCreateDir(t, fsys, "1/2")
+	assertCreateDir(t, fsys, "1/2/3")
+
+	testFS(t, fsys, fstest.MapFS{
+		"1/2/3": nil,
+	})
+}
+
+func testReadWriteFSCreateExistingDirectory(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	assertCreateDir(t, fsys, "tmp")
+	assertErrorIs(t, fsys.CreateDir("tmp", 0755), fs.ErrExist)
+}
+
+func testReadWriteFSCreateFileWithOpen(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	f0 := assertOpenFile(t, fsys, "foo", wasi.O_CREATE, 0644)
+	defer assertClose(t, f0)
+
+	// OK because O_EXCL is not set
+	f1 := assertOpenFile(t, fsys, "foo", wasi.O_CREATE, 0644)
+	defer assertClose(t, f1)
+}
+
+func testReadWriteFSCannotCreateExistingFile(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	f0 := assertOpenFile(t, fsys, "foo", wasi.O_CREATE, 0644)
+	assertClose(t, f0)
+
+	_, err := fsys.OpenFile("foo", wasi.O_CREATE|wasi.O_EXCL, 0644)
+	assertErrorIs(t, err, fs.ErrExist)
+}
+
+func testReadWriteFSTruncateFileWithOpen(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	f0 := assertOpenFile(t, fsys, "foo", wasi.O_RDWR|wasi.O_CREATE, 0644)
+	defer assertClose(t, f0)
+
+	assertWrite(t, f0, "Hello World!")
+
+	f1 := assertOpenFile(t, fsys, "foo", wasi.O_RDWR|wasi.O_TRUNC, 0)
+	defer assertClose(t, f1)
+
+	assertSeek(t, f0, 0, io.SeekStart)
+	b0 := assertRead(t, f0)
+	if len(b0) != 0 {
+		t.Errorf("original file still has access to content after truncation: %q", b0)
+	}
+
+	b1 := assertRead(t, f1)
+	if len(b1) != 0 {
+		t.Errorf("second file still has access to content after truncation: %q", b1)
+	}
+}
+
+func testReadWriteFSSetFileAccessTime(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	now := time.Now().Add(time.Hour)
+	assertCreateDir(t, fsys, "tmp")
+	assertSetFileTimes(t, fsys, "tmp", now, time.Time{})
+}
+
+func testReadWriteFSSetFileModTime(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	now := time.Now().Add(time.Hour)
+	assertCreateDir(t, fsys, "tmp")
+	assertSetFileTimes(t, fsys, "tmp", time.Time{}, now)
+}
+
+func testReadWriteFSSetFileTimes(t *testing.T, newFS MakeReadWriteFS) {
+	fsys, closeFS := assertNewFS(t, newFS)
+	defer assertCloseFS(t, closeFS)
+
+	now := time.Now().Add(time.Hour)
+	assertCreateDir(t, fsys, "tmp")
+	assertSetFileTimes(t, fsys, "tmp", now, now)
+}
